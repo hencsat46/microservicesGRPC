b7c100401ab09f7db08e4f58e11772b51c214ad3
hencsat46 <name66437@gmail.com>
2024-02-16 12:56:18 +0300

    shit fuck

--- library/internal/handler/handler.go
+++ library/internal/handler/handler.go
@@ -1,1 +1,14 @@
 package handler
+
+import (
+	library "github.com/hencsat46/protos/gen/go/library"
+)
+
+type handler struct {
+	library.UnimplementedLibraryServiceServer
+	usecase UsecaseInterfaces
+}
+
+type UsecaseInterfaces interface {
+	
+}
